7157477deff4b159e52de104e070ea7efb066438
AnibalFuentes <anibaldfuentesprieto@gmail.com>
2025-05-17 11:41:31 -0500

    create endpoind create, and setup class validator, adn use validation pipe

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsOptional()
+  @IsString()
+  firstName: string;
+
+  @IsOptional()
+  @IsString()
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsBoolean()
+  blocked: boolean;
+}
